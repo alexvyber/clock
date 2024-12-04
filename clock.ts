8040ea3b3bdf894bbe817ba88894a152d27ecbfb
Alexey Sokolov <alexvyber@gmail.com>
2024-12-04 13:54:44 +0300

    format

--- clock.ts
+++ clock.ts
@@ -1,22 +1,19 @@
 import { exec as execChildProcess } from "node:child_process";
 import { DateTime, Duration } from "luxon";
-import { promisify } from 'node:util';
+import { promisify } from "node:util";
 const exec = promisify(execChildProcess);
 
-const { stdout, stderr } = await exec('git branch');
+const { stdout, stderr } = await exec("git branch");
 
-if(stderr) throw new Error(stderr)
+if (stderr) throw new Error(stderr);
 
-console.log()
-const branch = stdout.split("\n").find(s => s.startsWith("* "))?.replaceAll("* ", "")
-
+const branch = stdout
+	.split("\n")
+	.find((s) => s.startsWith("* "))
+	?.replaceAll("* ", "");
 
 const start = DateTime.now();
 const label = branch ?? process.argv[2];
-
-
-
-
 
 setInterval(() => {
 	console.clear();
